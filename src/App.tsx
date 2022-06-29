632b627bca55960618076ede60323d6882a57931
tavs <82376053+7iquid@users.noreply.github.com>
2022-06-29 16:17:49 +0800

    set async

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './test/_custom.scss';
+import {GetRequestAsyncAwait} from './request/requestko'
 
 function App() {
   return (
     <div className="App">
         <h1> helo </h1>
+        <GetRequestAsyncAwait />
     </div>
   );
 }
