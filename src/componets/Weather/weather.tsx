451d009dc4ccf97d910f3a0ebc64bd4844a8129d
tavs <82376053+7iquid@users.noreply.github.com>
2022-07-12 19:17:29 +0800

    update scss

--- src/componets/Weather/weather.tsx
+++ src/componets/Weather/weather.tsx
@@ -4,7 +4,9 @@ import Grid from "./Grid/Grid";
 import GridRuler from "./GridRuler/GridRuler";
 import GetWeatherData,{WeatherDataGet}from '../API/weather_query';
 import Row from '../request/test';
-
+import styles from "./weather.module.scss";
+// import './weathers.scss';
+// import styles from "./Box.module.scss";
 
 
 
@@ -14,38 +16,13 @@ function Weather() {
   // let current:string = data.state
 
   return (
-    <div
-      className="Weather"
-      style={{ 
-      margin: "auto", 
-      marginTop: "60px",
-      position: "relative", 
-      height: "100vh",
-      width: "50%",
-      flex :'center',
-
-       }}
-    >
-    <Grid>
-    <Box >
-      asdwasd
-    </Box>
-    <Box >
-      asdwasd
-    </Box>
-    </Grid>
-
-  {/*        <Box>
-{cards?.items.map((card: ICardItem) => {
-         return <WeatherDataGet
-          key={data.id}
-          id={data.id}
-          img={data.img}
-          gender={data.gender}
-          name={data.name}
-         />;
-    })}        
-          </Box>*/}
+    <div   className={styles.style}  >
+      <Box >
+        Elit in reprehenderit nisi occaecat occaecat eu ullamco ea proident sunt cillum cillum irure ullamco proident proident aliquip sunt qui sit est eiusmod adipisicing.
+      </Box>
+      <Box >
+        asdwasd
+      </Box>
 
     </div>
   );
