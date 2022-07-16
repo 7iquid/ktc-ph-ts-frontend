d14b3aaff1e0c8b1330814a8359651b846c55425
tavs <82376053+7iquid@users.noreply.github.com>
2022-07-16 17:17:23 +0800

    intigrate api to gui

--- src/componets/API/weather_query.tsx
+++ src/componets/API/weather_query.tsx
@@ -1,38 +1,45 @@
 import React,{useState, useEffect, FunctionComponent} from "react";
 import axios from "axios";
 
+interface location2{
+    data: {
+        location:{
+            country: string;
+            lat: number;
+            localtime: Date;
+            localtime_epoch: number;
+            lon: number;
+            name: string;
+            region: string;
+            tz_id: string;
+            }
+        }
+    }
 
+// return (<ul>{Object.keys(data1).map((key,index)=><li>{key} :  {data1[key]}</li>)}</ul>)
 export function WeatherDataGet(){
-    const [data1, setdata1] = useState<Array<string>>([]);
+    const [data1, setdata1] = useState<any>(false);
+    const [loading, setLoading] = useState(true)
     useEffect(() =>{
-        async function fetchData(){
-            let fetchUrl ='https://ktc-ph-api.herokuapp.com/?format=json'
-            setdata1(await axios.get(fetchUrl));
-            return 'ok';
-        }
-        fetchData();
+       axios 
+        .get<any>('https://ktc-ph-api.herokuapp.com/?format=json')
+        .then(res =>{
+            setLoading(false)
+            setdata1(res.data.location)
+            console.log(res.data.location)
+        })
+        .catch(err =>{
+            setLoading(false)
+            console.log(err)
+        });
+              
     },[]);
-    return data1
-}
 
-const GetWeatherData:FunctionComponent<{
-}>=({
-})=>{ 
-    const [data1, setdata1] = useState(0);
-    useEffect(() =>{
-        async function fetchData(){
-            let fetchUrl ='https://ktc-ph-api.herokuapp.com/?format=json'
-            // const setdata1 = await axios.get(fetchUrl);
-            setdata1(await axios.get(fetchUrl));
-            // const [wearablesList, request] = useState<Props[]>([]);
-            // console.log(data1)
-            return data1;
-        }
-        fetchData();
-    },[]);
-    return<>
-        
-    </>
-} 
 
-export default GetWeatherData;
+    if(loading){return <>loading</>
+    }else{return (
+        <ul>{Object.keys(data1).map((key,index)=><li key={key}>{key} :  {data1[key]}</li>)}</ul>
+        )};  
+
+    }
+
--- src/componets/Weather/weather.tsx
+++ src/componets/Weather/weather.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Box from "./Box/Box";
 import Grid from "./Grid/Grid";
 import GridRuler from "./GridRuler/GridRuler";
-import GetWeatherData,{WeatherDataGet}from '../API/weather_query';
+import {WeatherDataGet} from '../API/weather_query';
 import Row from '../request/test';
 import styles from "./weather.module.scss";
+
+// import Users from '../test/testaxiox';
 // import './weathers.scss';
 // import styles from "./Box.module.scss";
 
 
-
 function Weather() {
-  // console.log(Day())
-  let data = WeatherDataGet()
-  // let current:string = data.state
-
+  let namess = '111111111'
   return (
     <div   className={styles.style}  >
       <Box >
-        Elit in reprehenderit nisi occaecat occaecat eu ullamco ea proident sunt cillum cillum irure ullamco proident proident aliquip sunt qui sit est eiusmod adipisicing.
+        <WeatherDataGet/>
       </Box>
       <Box >
+        
         asdwasd
+    
       </Box>
 
     </div>
