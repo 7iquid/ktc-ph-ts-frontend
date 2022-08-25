ed11a7201fe07cc678e87171a1ab75308d9bd0eb
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-25 11:22:00 +0800

    good mod

--- src/Template/Additems/Add/add.tsx
+++ src/Template/Additems/Add/add.tsx
@@ -6,25 +6,7 @@ import axios from 'axios'
 
 
 export function AddItem() {
-  const [forms, setForms] = useState<any>()
 
-   useEffect(()=>{
-          axios.get('http://127.0.0.1:8000/dtc/')
-          .then(function (response) {
-          // handle success
-          setForms(response)
-
-          // console.log(response.data);
-          })
-          .catch(function (error) {
-          // handle error
-          console.log(error);
-          })
-          .then(function () {
-          // always executed
-          });
-  console.log(forms)
-   },[])       
   return <>{}</>
 
 
--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -1,6 +1,6 @@
 import styles from './Home.module.scss'
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useContext } from 'react';
+import { ApiContext } from '../../componets/API/ApiProvider';
 // export function Home() {
 //   return (
 //     <div className={styles.main}>
@@ -64,19 +64,21 @@ const Slideshow = ({ imgs }:any) => {
 }
 
 export function Home() {
-
+  let {newsfeed} =useContext(ApiContext)
   return (
     <div className={styles.main}>
       <h1>SLIDES SHOW</h1>
       <Slideshow
         imgs={[
+          
           'https://images.unsplash.com/photo-1585255318859-f5c15f4cffe9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
           'https://images.unsplash.com/photo-1584226761916-3fd67ab5ac3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
           'https://images.unsplash.com/photo-1585179292338-45ba1f62f082?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
           'https://images.unsplash.com/photo-1584753987666-ead137ec0614?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
           'https://images.unsplash.com/photo-1584691267914-91c0bee55964?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
           'https://images.unsplash.com/photo-1585084335487-f653d0859c14?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-        ]}
+
+        ].concat(newsfeed?.map((a:any)=> a?.photo))}
       />
     </div>
   );
