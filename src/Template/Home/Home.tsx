e32bee6f50a9af81d60ec27a76062e70268efe8d
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-25 22:09:18 +0800

    add slideshow

--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -1,87 +1,82 @@
 import styles from './Home.module.scss'
 import React, { useState, useEffect, useContext } from 'react';
 import { ApiContext } from '../../componets/API/ApiProvider';
-// export function Home() {
-//   return (
-//     <div className={styles.main}>
-//       <h1 >Welcome to React Router!</h1>
-//     </div>
-//   );
-// }
+
+import { Fade  } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const fadeImages = [
+  {
+    url: 'https://images.unsplash.com/photo-1585255318859-f5c15f4cffe9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1584226761916-3fd67ab5ac3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1585179292338-45ba1f62f082?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+    caption: 'Slide 3'
+  },
+];
 
 
 
-
-const Thumbnail = ({ arr, image, index }:any) => {
-  return (<div className="tumbnail">
-    {
-      arr.map((imgsrc:any, i:any) => (
-        <img
-          key={i}
-          height="50"
-          src={imgsrc}
-          onClick={() => image(i)}
-          className={index === i ? 'active' : ''}
-        />
-      ))
-    }
-  </div>)
-}
-
-const Slideshow = ({ imgs }:any) => {
-  const [index, setIndex] = useState(0)
-
-  useEffect(() => {
-    setIndex(0)
-  }, [])
-
-  const next = () => {
-    if (index === imgs.length - 1) {
-      setIndex(0)
-    } else {
-      setIndex(index + 1)
-    }
-  }
-  const prev = () => {
-    if (index === 0) {
-      setIndex(imgs.length - 1)
-    } else {
-      setIndex(index - 1)
-    }
-  }
-
-
+export const Home = () => {
+  let {newsfeed} =useContext(ApiContext)
   return (
-    <div className="slideshow">
-      <img className="mainImg" src={imgs[index]} />
-      <div className="actions">
-        <button onClick={prev}>👈</button>
-        <button onClick={next}>👉</button>
-      </div>
-      <Thumbnail arr={imgs} image={setIndex} index={index} />
+    <div className={styles.main}>
+      <Fade>
+        {newsfeed.map((newsfeed:any, index:any) => (
+          <div className="each-fade" key={index}>
+            <div >
+              <img src={newsfeed.photo} />
+            </div>
+          
+          </div>
+        ))}
+      </Fade>
     </div>
   )
 }
 
-export function Home() {
-  let {newsfeed} =useContext(ApiContext)
-  return (
-    <div className={styles.main}>
-      <h1>SLIDES SHOW</h1>
-      <Slideshow
-        imgs={[
+
+
+
+
+
+
+
+
+
+
+
+// export function Home() {
+//   let {newsfeed} =useContext(ApiContext)
+//   return (
+//     <div className={styles.main}>
+//       <h1>SLIDES SHOW</h1>
+//       <Slideshow
+//         imgs={[
           
-          'https://images.unsplash.com/photo-1585255318859-f5c15f4cffe9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-          'https://images.unsplash.com/photo-1584226761916-3fd67ab5ac3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-          'https://images.unsplash.com/photo-1585179292338-45ba1f62f082?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-          'https://images.unsplash.com/photo-1584753987666-ead137ec0614?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-          'https://images.unsplash.com/photo-1584691267914-91c0bee55964?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
-          'https://images.unsplash.com/photo-1585084335487-f653d0859c14?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1585255318859-f5c15f4cffe9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1584226761916-3fd67ab5ac3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1585179292338-45ba1f62f082?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1584753987666-ead137ec0614?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1584691267914-91c0bee55964?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
+//           'https://images.unsplash.com/photo-1585084335487-f653d0859c14?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixlib=rb-1.2.1&q=80&w=500',
 
-        ].concat(newsfeed?.map((a:any)=> a?.photo))}
-      />
-    </div>
-  );
-}
+//         ].concat(newsfeed?.map((a:any)=> a?.photo))}
+//       />
+//     </div>
+//   );
+// }
 
-// export default Home;
+// // export default Home;
+
+
+
+
+
+
+
