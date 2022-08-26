46c09f21759e944b549285fff28c532a2eee1b85
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-26 17:42:39 +0800

    test

--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -2,6 +2,7 @@ import styles from './Home.module.scss'
 import React, { useState, useEffect, useContext } from 'react';
 import { ApiContext } from '../../componets/API/ApiProvider';
 import { Fade } from 'react-slideshow-image';
+import { removeFromLocal } from '../../componets/API/LocalStorageApi';
 import 'react-slideshow-image/dist/styles.css'
 
 const newsfeedsc = [
@@ -12,12 +13,21 @@ const newsfeedsc = [
 ];
 
 
+// function useRefreshState(urlNewsfeeds){
+//   const [refresh , setUrlNewsfeedsrefresh] = useState(()=>{
+//      const ok = 'ok'
+//      return ok
+//   })
+//   return [refresh , setUrlNewsfeedsrefresh]
+// }
+
 
 export function Home() {
-  let {newsfeed} =useContext(ApiContext)
+  let {newsfeed, urlNewsfeeds} =useContext(ApiContext)
+  // const [urlNewsfeedsrefresh, setUrlNewsfeedsrefresh] = useRefreshState(urlNewsfeeds )
   // const [newsfeed, setNewsfeed] = useState(newsfeedsc)
-
   // useEffect(()=>{setNewsfeed(newsfeeds)},[newsfeeds])
+    // <img src={newsfeed.photo} className={styles.photo} onError={setUrlNewsfeedsrefresh}/>
   return (
     <div className={styles.main}>
       {/*<h1>SLIDES SHOW</h1>*/}
@@ -25,9 +35,9 @@ export function Home() {
           {newsfeed?.map((newsfeed:any, index:any) => (
             <div className="each-fade" key={index}>
               <div className="image-container">
-                <img src={newsfeed.photo} className={styles.photo}/>
+                <img src={newsfeed.photo} className={styles.photo} />
               </div>
-              <h2>{newsfeed.name}</h2>
+              <h2>{newsfeed.name} { newsfeed?.date}</h2>
             </div>
           ))}
         </Fade>
