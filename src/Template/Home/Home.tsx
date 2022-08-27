d3a704e5c8b89d8073e263ecf7e1c397d74c9810
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-27 17:43:16 +0800

    debug

--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -4,18 +4,21 @@ import { ApiContext } from '../../componets/API/ApiProvider';
 import { Fade } from 'react-slideshow-image';
 import { removeFromLocal } from '../../componets/API/LocalStorageApi';
 import 'react-slideshow-image/dist/styles.css'
+import CircularProgress from '@mui/material/CircularProgress';
 const JsonFind = require("json-find");
-
 
 
 export function Home() {
   let {newsfeed} =useContext(ApiContext)
-  let data = newsfeed[1]
+  let renderit = false
+  if(newsfeed){ newsfeed =newsfeed[1]; renderit =true}
+  console.log(newsfeed)
   return (
     <div className={styles.main}>
       {/*<h1>SLIDES SHOW</h1>*/}
+      {renderit ?
         <Fade>
-          {data?.map((data:any, index:any) => (
+          {newsfeed?.map((data:any, index:any) => (
             <div className="each-fade" key={index}>
               <div className="image-container">
                 <img src={data.photo} className={styles.photo} />
@@ -24,6 +27,7 @@ export function Home() {
             </div>
           ))}
         </Fade>
+        : <CircularProgress/>}
     </div>
   );
 }
