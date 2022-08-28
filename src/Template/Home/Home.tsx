a11d2579b55bd34a024d77bc4bd28b7564d24ae4
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-28 15:57:23 +0800

    ok na news feeds

--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -5,34 +5,40 @@ import { Fade } from 'react-slideshow-image';
 import { removeFromLocal } from '../../componets/API/LocalStorageApi';
 import 'react-slideshow-image/dist/styles.css'
 import CircularProgress from '@mui/material/CircularProgress';
+
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid  from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 const JsonFind = require("json-find");
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    }));
 
 export function Home() {
   return(
-    <Grid  display=' flex'>
-      <Grid item xs={6} md={8}>
-
-                 <p>Proident aliquip sed voluptate 
-        nullaaa
-        sedasd
-        asd nostrud voluptate deserunt dolor ullamco fugiat anim ea et magna qui dolor excepteur et reprehenderit fugiat proident in excepteur quis aute.</p>
-
+    <div className={styles.main}>
+      <Grid container spacing={2}>
+        
+        <Grid item xs={6} md={10} display='flex'>
+            <CathergoriesKo/>
+          <Grid item xs={10} md={6}>
+            <NewsFeeds/>
+          </Grid>
+        </Grid>
 
       </Grid>
-      <Grid item xs={6} md={8}>
-        {/*<NewsFeeds/>*/}
-        <Fade>    
-          <p>Proident aliquip sed voluptate 
-          nulla nostrud voluptate deserunt dolor ullamco fugiat anim ea et magna qui dolor excepteur et reprehenderit fugiat proident in excepteur quis aute.</p>
-        </Fade>   
-      </Grid>
-    </Grid> 
+    </div>
   )
 }
+
 
 
 
@@ -49,11 +55,21 @@ function NewsFeeds(){
               <div className="image-container">
                 <img src={data.photo} className={styles.photo} />
               </div>
-              <h2>{data.name} { data?.date}</h2>
             </div>
           ))}
         </Fade>
         : <CircularProgress/>}
     </div>
   );
+}
+
+
+function CathergoriesKo() {
+  return(
+    <div className={styles.main}>
+      <h3>ELECTRICAL PARTS</h3>  
+      <h3>FABRICATION PARTS</h3> 
+      <h3>ELECTRONICS PARTS</h3> 
+    </div>
+  )
 }
