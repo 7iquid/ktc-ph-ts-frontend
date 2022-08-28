eb7f2530b6116219a872aef75f93e96c0c65c992
tavs <82376053+7iquid@users.noreply.github.com>
2022-08-28 15:07:15 +0800

    1

--- src/Template/Home/Home.tsx
+++ src/Template/Home/Home.tsx
@@ -5,16 +5,42 @@ import { Fade } from 'react-slideshow-image';
 import { removeFromLocal } from '../../componets/API/LocalStorageApi';
 import 'react-slideshow-image/dist/styles.css'
 import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid  from '@mui/material/Grid';
 const JsonFind = require("json-find");
 
 
 export function Home() {
-  let {newsfeed} =useContext(ApiContext)
-  let renderit = false
-  if(newsfeed){ newsfeed =newsfeed[1]; renderit =true}
-  console.log(newsfeed)
+  return(
+    <Grid  display=' flex'>
+      <Grid item xs={6} md={8}>
+
+                 <p>Proident aliquip sed voluptate 
+        nullaaa
+        sedasd
+        asd nostrud voluptate deserunt dolor ullamco fugiat anim ea et magna qui dolor excepteur et reprehenderit fugiat proident in excepteur quis aute.</p>
+
+
+      </Grid>
+      <Grid item xs={6} md={8}>
+        {/*<NewsFeeds/>*/}
+        <Fade>    
+          <p>Proident aliquip sed voluptate 
+          nulla nostrud voluptate deserunt dolor ullamco fugiat anim ea et magna qui dolor excepteur et reprehenderit fugiat proident in excepteur quis aute.</p>
+        </Fade>   
+      </Grid>
+    </Grid> 
+  )
+}
+
+
+
+function NewsFeeds(){
+   let {newsfeed,renderit = false} =useContext(ApiContext)
+  if(newsfeed){ newsfeed =newsfeed?.responsedata; renderit =true}
   return (
-    <div className={styles.main}>
+    <div className={styles.newsfeed}>
       {/*<h1>SLIDES SHOW</h1>*/}
       {renderit ?
         <Fade>
@@ -31,5 +57,3 @@ export function Home() {
     </div>
   );
 }
-
-// export default Home;
